439355188a8e2231945c0df7192af6b81221b5a9
anjalipr <anjalipr647@gmail.com>
2024-12-19 03:18:49 +0000

    update02

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,9 @@
-console.log("Jai Shri Ram !!")
+const accountId=144553
+
+let accountEmail="anjali@gmail.com"
+var accountPassword="12345"
+accountCity="pune"
+
+accountId=2
+console.log(accountId)
+accountEmail
